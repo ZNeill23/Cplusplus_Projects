09bcbee541d205478ad73711c5e382b5763b4c0e
Zack <zack.neill2012@gmail.com>
2025-03-08 06:35:34 -0500

    Added Lesson 1: Basic C++ Syntax with .gitignore

---
+++ lesson1_cpp_syntax.cpp
@@ -0,0 +1,11 @@
+// Lesson 1: C++ Syntax - Created using W3Schools concepts
+
+#include <iostream> // Allows us to use cout for output
+
+int main() {
+    // Print a welcome message to the console
+    std::cout << "Welcome to C++ Programming!" << std::endl;
+    std::cout << "This is our first lesson on C++ syntax." << std::endl;
+
+    return 0; // Indicates successful program execution
+}
